3d7998ad25d10d94b77e49ec174deb28922cdfb8
Arthur <peraarthur2018@gmail.com>
2022-03-19 21:25:30 -0300

    third commit

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 
 function multiplication(){
+    
+   
+    
+    
 
 let mecmec = document.getElementsByClassName("resultado");
 
@@ -8,58 +12,63 @@ let mecmec = document.getElementsByClassName("resultado");
     
     var input2 = document.getElementById("m2").value;
 
+    
      var calculando = input1 * input2; 
 
-if(input1 == "" && input2 == ""){
-    mecmec[0].innerHTML = ("Please, put you number.")
-}
-else{
-    mecmec[0].innerHTML = (calculando)
-}
+   
+     
+        mecmec[0].innerHTML = (calculando)
+        if(input1 == 0 && input2 == 0){
+            mecmec[0].innerHTML = ("Please, put a number.")
+        } 
 }
 
 function subtraction(){
-
     let mecmec = document.getElementsByClassName("resultado1");
     
+    var input3 = document.getElementById("m3").value;
     
-        var input3 = document.getElementById("m3").value;
-        
-        var input4 = document.getElementById("m4").value;
-    
-         var calculando = input3 - input4; 
-    
+    var input4 = document.getElementById("m4").value;
+
+  
+
+    var calculando = input3 - input4; 
+   
+         
+
     if(input3 == "" && input4 == ""){
-        mecmec[0].innerHTML = ("Please, put you number.")
-    }
-    else{
-        mecmec[0].innerHTML = (calculando)
-    }
+        mecmec[0].innerHTML = ("Please, put a number.")
     }
 
+        else{mecmec[0].innerHTML = (calculando)}
+
+}
+        
+    
+    
     function sum(){
 
         let mecmec = document.getElementsByClassName("resultado2");
         
+        let input5 = document.getElementById("m5").value;
         
-            var input5 = document.getElementById("m5").value;
-            
-            var input6 = document.getElementById("m6").value; 
-        
-            var input5 = parseInt(input5);
-            
-            var input6 = parseInt(input6);
+        let input6 = document.getElementById("m6").value; 
 
-             var calculando = input5 + input6; 
+        mecmec[0].innerHTML = (calculando)
+        if(input5 == 0 && input6 == 0){
+            mecmec[0].innerHTML = ("Please, put a number.")
+        } 
         
-        if(input5 == "" && input6 == ""){
-            mecmec[0].innerHTML = ("Please, put you number.")
-        }
-        else{
-            mecmec[0].innerHTML = (calculando)
-        }
-        }
+        const num5 = parseInt(input5);
+        
+        const num6 = parseInt(input6);
 
+             var calculando = num5 + num6; 
+             typeof num5, typeof num6;
+            
+             mecmec[0].innerHTML = (calculando) 
+            
+        }
         function division(){
 
             let mecmec = document.getElementsByClassName("resultado3");
@@ -72,7 +81,7 @@ function subtraction(){
                  var calculando = input7 / input8; 
             
             if(input7 == "" && input8 == ""){
-                mecmec[0].innerHTML = ("Please, put you number.")
+                mecmec[0].innerHTML = ("Please, put a number.")
             }
             else{
                 mecmec[0].innerHTML = (calculando)
